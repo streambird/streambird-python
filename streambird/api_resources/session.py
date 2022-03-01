b095c2232622edfdf4e00d12a0a0c119fae11739
Timothy Chung <timchunght@gmail.com>
2022-02-28 21:35:22 -0500

    add delete sessions

--- streambird/api_resources/session.py
+++ streambird/api_resources/session.py
@@ -6,7 +6,7 @@ class Session:
 
     def __init__(self, client):
         self._client = client
-        
+
     def verify(
         self,
         session_token: str,
@@ -17,7 +17,7 @@ class Session:
         }
         if session_expires_in:
             req['session_expires_in'] = session_expires_in
-        
+
         return self._client.api.post_request('auth/sessions/verify', body=req)
 
     def list(
@@ -27,5 +27,19 @@ class Session:
         params = {
             'user_id': user_id,
         }
-        
+
         return self._client.api.get_request('auth/sessions/list', params=params)
+
+    def delete(
+        self,
+        session_token: Optional[str] = None,
+        session_id: Optional[str] = None,
+    ):
+        req = {}
+        if session_token:
+            req['session_token'] = session_token
+
+        if session_id:
+            req['session_id'] = session_id
+
+        return self._client.api.delete_request('auth/sessions/delete', body=req)
